35d3de2fe217d1c7761b346955c7c93269ab947c
Xnan-blop <xnan6@gatech.edu>
2021-11-22 18:53:29 -0500

    Create AdoptPage.jsx

---
+++ src/screens/Adopt/AdoptPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./AdoptPage.module.css";
+
+const AdoptPage = ({animals}) => {
+    console.log(animals)
+        return (
+        <div className={classes.centertext}>
+          <h1> Browse Shelter Animals </h1>
+            <p>animal</p>
+              
+            
+          </div>
+      );
+        }
+
+export default AdoptPage;
