eaa75de10704f6e3ee597651201381ebfaafa4cc
Xnan-blop <xnan6@gatech.edu>
2021-11-30 18:12:35 -0500

    Update AdoptPage.jsx

--- src/screens/Adopt/AdoptPage.jsx
+++ src/screens/Adopt/AdoptPage.jsx
@@ -1,16 +1,58 @@
-import React from "react";
-import classes from "./AdoptPage.module.css";
 
-const AdoptPage = ({animals}) => {
-    console.log(animals)
-        return (
-        <div className={classes.centertext}>
-          <h1> Browse Shelter Animals </h1>
-            <p>User should be able to view a list of cats when they click on the Adopt tab on the nav bar. The list should display a list of all the cats in the shelter’s DB. You need to hardcode cat data in your database</p>
-              
-            
-          </div>
-      );
-        }
+import style from "./AdoptPage.module.css";
+import React, {useState} from 'react'
+export default function AdoptPage() {
+    const [name,setname]=useState('')
+    const [Email,setEmail]=useState('')
+    const [phone,setphone]=useState('')
+    const [location,setlocation]=useState('')
 
-export default AdoptPage;
+    async function submitHandler(e){
+        e.preventDefault()
+        const res=await fetch('http://localhost:3000/ssr',{
+            method:'POST', 
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                name:name,
+                Email:Email,
+                phone:phone,
+                location:location,
+                img:'/image/caty.gif'
+            })
+        })
+
+
+
+    }
+  
+    return (
+      <div style={{ 
+        backgroundImage: `url(image/cloud.gif)` ,
+        backgroundRepeat: 'no-repeat',
+        
+      }}>
+  
+        <form onSubmit={submitHandler}>
+           <h1 className={style.title}><center>~Register For An Animal~</center></h1>
+           <center><label className={style.txt} htmlFor="name">
+           Name</label>
+           <input className={style.enter} required='true'type="text" onChange={e=>setname(e.target.value)}/></center>
+           <center><label className={style.txt}htmlFor="species">
+           Email</label>
+           <input className={style.enter}required='true'type="text" onChange={e=>setspecies(e.target.value)}/></center>
+           <center><label className={style.txt}htmlFor="description">
+           Phone#</label>
+           <input className={style.enter} required='true'type="text" onChange={e=>setdescription(e.target.value)}/></center>
+           <center><label className={style.txt}htmlFor="location">
+           Location</label>
+           <input className={style.enter} required='true' type="text" onChange={e=>setlocation(e.target.value)}/></center>
+           
+           <center><button className={style.enter}className={style.submit}type="submit">submit</button></center>
+           
+         </form>
+      
+      </div>
+    )
+  }
