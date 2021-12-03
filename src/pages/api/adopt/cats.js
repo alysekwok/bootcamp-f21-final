458d8631be35f878aaecbcf6f3f8df93fa8d2beb
alysekwok <ALYSEKWOKK@GMAIL.COM>
2021-12-02 19:33:28 -0500

    add hard coded cat data

---
+++ src/pages/api/adopt/cats.js
@@ -0,0 +1,46 @@
+import Cats from "../../server/models/Cats"
+
+const bart = Cats.create({
+    name: "Bartholomew",
+    image: google.com,
+    age: 5,
+    location: "Cabbagetown",
+    breed: "Siamese",
+    isAdopted: false
+})
+
+const bart = Cats.create({
+    name: "Alyse",
+    image: google.com,
+    age: 1,
+    location: "Johns Creek",
+    breed: "Burmese",
+    isAdopted: false
+})
+
+const bart = Cats.create({
+    name: "Harriet",
+    image: google.com,
+    age: 3,
+    location: "Suwon",
+    breed: "Bombay",
+    isAdopted: false
+})
+
+const bart = Cats.create({
+    name: "Ewan",
+    image: google.com,
+    age: 2,
+    location: "Kansas City",
+    breed: "Bengal",
+    isAdopted: false
+})
+
+const bart = Cats.create({
+    name: "Nancy",
+    image: google.com,
+    age: 10,
+    location: "Chino",
+    breed: "Chartreux",
+    isAdopted: false
+})
