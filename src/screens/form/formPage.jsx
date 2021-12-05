c3e0ad6be92b8e7cbd01a3a46043ade945be12a5
Xnan-blop <xnan6@gatech.edu>
2021-12-05 12:32:42 -0500

    Update formPage.jsx

--- src/screens/form/formPage.jsx
+++ src/screens/form/formPage.jsx
@@ -1,13 +1,21 @@
-import style from "./AdoptPage.module.css";
+import style from "./FormPage.module.css";
 import React, {useState} from 'react'
-export default function AdoptPage() {
+export default function FormPage() {
     const [name,setname]=useState('')
     const [Email,setEmail]=useState('')
     const [phone,setphone]=useState('')
     const [location,setlocation]=useState('')
 
     async function submitHandler(e){
-        e.preventDefault()
+      e.preventDefault()
+      console.log(phone)
+      if(phone.match(/^[0-9]+$/) == null) {
+        alert("can only contain digits")
+      }
+      if(Email.match(/\S+@\S+\.\S+/) == null) {
+        alert("must be valid email address")
+      }
+      try {
         const res=await fetch('http://localhost:3000/ssr',{
             method:'POST', 
             headers:{
@@ -18,14 +26,20 @@ export default function AdoptPage() {
                 Email:Email,
                 phone:phone,
                 location:location,
-                img:'/image/caty.gif'
             })
         })
-
-
-
+      } catch (e) {
+        alert("Error!")
+      }
+      
     }
-  
+
+        
+
+
+
+    
+
     return (
       <div style={{ 
         backgroundImage: `url(image/cloud.gif)` ,
@@ -40,10 +54,10 @@ export default function AdoptPage() {
            <input className={style.enter} required='true'type="text" onChange={e=>setname(e.target.value)}/></center>
            <center><label className={style.txt}htmlFor="species">
            Email</label>
-           <input className={style.enter}required='true'type="text" onChange={e=>setspecies(e.target.value)}/></center>
+           <input className={style.enter}required='true'type="text" onChange={e=>setEmail(e.target.value)}/></center>
            <center><label className={style.txt}htmlFor="description">
            Phone#</label>
-           <input className={style.enter} required='true'type="text" onChange={e=>setdescription(e.target.value)}/></center>
+           <input className={style.enter} required='true'type="text" onChange={e=>setphone(e.target.value)}/></center>
            <center><label className={style.txt}htmlFor="location">
            Location</label>
            <input className={style.enter} required='true' type="text" onChange={e=>setlocation(e.target.value)}/></center>
