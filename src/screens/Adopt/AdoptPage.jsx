91be435a704abdb94c9c090005c9f9f0aee635a0
Xnan-blop <xnan6@gatech.edu>
2021-12-05 12:32:36 -0500

    Update AdoptPage.jsx

--- src/screens/Adopt/AdoptPage.jsx
+++ src/screens/Adopt/AdoptPage.jsx
@@ -1,71 +1,20 @@
-import style from "./AdoptPage.module.css";
-import React, {useState} from 'react'
-export default function AdoptPage() {
-    const [name,setname]=useState('')
-    const [Email,setEmail]=useState('')
-    const [phone,setphone]=useState('')
-    const [location,setlocation]=useState('')
+import React from 'react'
+import style from './AdoptPage.module.css'
+import CatCard from '../../components/CatCard/CatCard'
 
-    async function submitHandler(e){
-      e.preventDefault()
-      console.log(phone)
-      if(phone.match(/^[0-9]+$/) == null) {
-        alert("can only contain digits")
-      }
-      if(Email.match(/\S+@\S+\.\S+/) == null) {
-        alert("must be valid email address")
-      }
-      try {
-        const res=await fetch('http://localhost:3000/ssr',{
-            method:'POST', 
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify({
-                name:name,
-                Email:Email,
-                phone:phone,
-                location:location,
-            })
-        })
-      } catch (e) {
-        alert("Error!")
-      }
-      
-    }
-
-        
-
-
-
-    
-
-    return (
-      <div style={{ 
-        backgroundImage: `url(image/cloud.gif)` ,
-        backgroundRepeat: 'no-repeat',
-        
-      }}>
-  
-        <form onSubmit={submitHandler}>
-           <h1 className={style.title}><center>~Register For An Animal~</center></h1>
-           <center><label className={style.txt} htmlFor="name">
-           Name</label>
-           <input className={style.enter} required='true'type="text" onChange={e=>setname(e.target.value)}/></center>
-           <center><label className={style.txt}htmlFor="species">
-           Email</label>
-           <input className={style.enter}required='true'type="text" onChange={e=>setEmail(e.target.value)}/></center>
-           <center><label className={style.txt}htmlFor="description">
-           Phone#</label>
-           <input className={style.enter} required='true'type="text" onChange={e=>setphone(e.target.value)}/></center>
-           <center><label className={style.txt}htmlFor="location">
-           Location</label>
-           <input className={style.enter} required='true' type="text" onChange={e=>setlocation(e.target.value)}/></center>
-           
-           <center><button className={style.enter}className={style.submit}type="submit">submit</button></center>
-           
-         </form>
-      
+export default function Adoptpage( {bart} ) {
+console.log(bart)
+  return (
+    <div className={style.container}>
+      <h1> Browse Shelter Animals </h1>
+      <div className={style.list}>
+        {
+          bart.map(element=>{
+            return <CatCard bart={element}></CatCard>
+          }
+          )
+        }
       </div>
-    )
+    </div>
+  )
   }
